7bf97ca4a2126ac0955657f18552d4d40b9fd5a5
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-15 21:04:34 -0300

    🐛fix(file): fix typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-	type: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	username: 'root',
-	password: 'Toor@1234',
-	database: 'db_todo',
-	entities: [__dirname + '/../**/*.entity.{js,ts}'],
-	synchronize: true,
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'root',
+    password: 'Toor@1234',
+    database: 'db_todo',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
 };
