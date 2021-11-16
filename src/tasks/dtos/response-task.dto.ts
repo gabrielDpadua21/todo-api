626353296e19c83764930ea06e08ed335fba70f9
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-11-15 21:02:14 -0300

    🐛fix(file): add id in task response dto

--- src/tasks/dtos/response-task.dto.ts
+++ src/tasks/dtos/response-task.dto.ts
@@ -1,4 +1,5 @@
 export class ResponseTaskDto {
+    id: number;
     name: string;
     description?: string;
     status: boolean;
@@ -6,12 +7,14 @@ export class ResponseTaskDto {
     updatedAt: Date;
 
     constructor(
+        id: number,
         name: string,
         status: boolean,
         createdAt: Date,
         updatedAt: Date,
         description?: string,
     ) {
+        this.id = id;
         this.name = name;
         this.status = status;
         this.createdAt = createdAt;
